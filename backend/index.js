c1a80d0a27cea60baa36a130a21251b9c64c5472
Daniel Ale <denielale9291@gmail.com>
2018-08-23 13:54:02 +0100

    Created the backend using Nodejs and Mongodb

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const config = require("./db");
+
+const users = require("./routes/user");
+mongoose
+  .connect(
+    config.DB,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database: " + err);
+  });
+
+const app = express();
+app.use(passport.initialize());
+require("./passport");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.use("/api/auth", users);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
